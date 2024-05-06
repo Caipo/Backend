2028c23e681096fa8a39677d2fac6682ebb49c8f
Matttt <cathcartmathew@gmail.com>
2024-05-05 21:21:58 -0600

    Added import module for typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot(
+          {
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'password',
+            database: 'tribal',
+            entities: [],
+            synchronize: true,
+          }
+      ),
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+
+}
