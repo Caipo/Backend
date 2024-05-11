3700d8aa56b4547ff12ca0d69c953a45a6ade592
Matttt <cathcartmathew@gmail.com>
2024-05-10 19:48:24 -0600

    Added paths

--- src/modules/user/api/user.mutations.controller.ts
+++ src/modules/user/api/user.mutations.controller.ts
@@ -4,12 +4,12 @@ import {Request, Response} from "express";
 
 @Controller(Paths.user.root)
 export class UserMutationsController {
-    @Post()
+    @Post(Paths.user.mutations.login)
     login(@Req() request: Request, @Res() response: Response) {
         response.send("Logged in");
     }
 
-    @Post()
+    @Post(Paths.user.mutations.createUser)
     createUser(@Req() request: Request, @Res() response: Response) {
         response.send("Created");
     }
