69efe1bbc0cd146d27110eb698a6b11143eccb6a
Matttt <cathcartmathew@gmail.com>
2024-05-19 20:26:06 -0600

    ESLINT Rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,35 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
-
 export default [
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  {
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      project: "tsconfig.json",
+      sourceType: "module",
+    },
+    plugins: ["@typescript-eslint/eslint-plugin"],
+    extends: [
+      "plugin:@typescript-eslint/recommended",
+      "prettier/@typescript-eslint",
+      "plugin:prettier/recommended",
+    ],
+    root: true,
+    env: {
+      node: true,
+      jest: true,
+    },
+    ignorePatterns: [".eslintrc.js"],
+    rules: {
+      "@typescript-eslint/interface-name-prefix": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      quotes: [
+        "error",
+        "double",
+        {
+          avoidEscape: true,
+          allowTemplateLiterals: false,
+        },
+      ],
+    },
+  },
 ];
