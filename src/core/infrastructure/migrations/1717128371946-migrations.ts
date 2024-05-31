a351f550104819cef5f9c71c76cab2c5a89feae2
Nicholas <nicholas@Nicholass-MacBook-Air.local>
2024-05-30 21:27:26 -0700

    added the migration

---
+++ src/core/infrastructure/migrations/1717128371946-migrations.ts
@@ -0,0 +1,24 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migrations1717128371946 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(`
+            CREATE TABLE "tribe_messages" (
+                "id" uuid NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+                "sender_id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+                "tribe_id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+                "message" character varying(280) NOT NULL,
+                "created_at" bigint NOT NULL,
+                CONSTRAINT "PK_message_id" PRIMARY KEY ("id")
+            )
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(`
+            DROP TABLE "tribe_messages"
+        `);
+    }
+
+}
