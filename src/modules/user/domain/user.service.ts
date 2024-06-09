029e825e204955f03d191cd6eb154f6b3ea0846a
Nicholas <nicholas@Nicholass-MacBook-Air.local>
2024-06-08 22:16:34 -0700

    added user functionaltys

--- src/modules/user/domain/user.service.ts
+++ src/modules/user/domain/user.service.ts
@@ -1,10 +1,11 @@
-import { ClassProvider, Inject, Injectable } from "@nestjs/common";
+import { ClassProvider, Inject, Injectable, HttpException, HttpStatus  } from "@nestjs/common";
 import {
 	ServiceCreateUserInput,
 	ServiceUser,
 	UserServiceDefinition,
 	UserServiceName,
 } from "src/modules/user/domain/user.service.types";
+import { profilePictures } from "src/modules/user/domain/pictures";
 import {
 	RepoUser,
 	UserRepositoryDefinition,
@@ -23,7 +24,23 @@ export class UserService implements UserServiceDefinition {
 	}
 
 	async createUser({ username, password }: ServiceCreateUserInput): Promise<ServiceUser> {
-		const repoUser: RepoUser = await this.userRepository.createUser({ username: username, password: password });
+		const createdAt: bigint = BigInt(Date.now());
+		const defaultBiography: string = "Defualt Bio";
+
+		const randomIndex: number = Math.floor(Math.random() * profilePictures.length);
+		const profilePictureUrl: string = profilePictures[randomIndex];
+        
+        if(await this.userRepository.checkUsernameTaken({username: username})){
+            throw new HttpException("Username Taken", HttpStatus.CONFLICT);
+        }
+
+		const repoUser: RepoUser = await this.userRepository.createUser({
+			username: username,
+			password: password,
+			createdAt: createdAt,
+			biography: defaultBiography,
+			profilePictureUrl: profilePictureUrl,
+		});
 
 		const serviceUser: ServiceUser = {
 			id: repoUser.id,
